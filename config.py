b9ee6d0646cd8923e3debbeaf504ef68263ce611
IldarGiniyatullin2409 <118752795+IldarGiniyatullin2409@users.noreply.github.com>
2022-12-30 22:33:18 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,10 @@
+user_token = 'токен пользователя'
+community_token = 'токен сообщества'
+
+offset = 0
+line = range(0, 1000)
+
+postgres_host = 'localhost'
+postgres_user = 'postgres'
+postgres_password = '1111111'
+postgres_db_name = 'postgres'
